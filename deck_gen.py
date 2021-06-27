62d5d6a084aec9fb753666d8069613cccc48e2b5
Amos Nimos <76786560+AmosNimos@users.noreply.github.com>
2021-06-27 10:21:14 -0400

    Update deck_gen.py
    Updated card name and list.
    Added some exception to if the amount of Card required is higher then the amount of card in the list.
    Feel free to add your own favourite card and make your own list.

--- deck_gen.py
+++ deck_gen.py
@@ -5,51 +5,54 @@
 from random import randrange as random
 
 
-##(Other forbidden/Limited card might still be found, if so place them in the list bellow.)
-#Frobidden = ["Change fo Hearth",]
-Limited_spell = ["Sword of revealing light","Premature burial","Snatch steal"]
-Limited_trap = ["Call of the haunted","Magic Cylinder"]
+##(Other forbidden/Limited card might still be found, if so place them here.)
+
+Limited = True
+Frobidden_spell = ["Change fo Hearth", "Dark Hole", "Monster Reborn",]
+Limited_spell = ["Sword of Revealing Light","Premature Burial","Snatch Steal"]
+Limited_trap = ["Call of the haunted","Magic Cylinder","Mirror Force","Torrential tribute"]
 
 Monster = [
 "Marshmallon",
 "Penguin Soldier",
-"Goblin attack force",
+"Goblin Attack Force",
 "Goblin elite attack force",
 "Jinzo",
-"Slate warrior",
-"Skull mark ladybug",
-"Des kangaroo",
-"Des koala",
-"Giant rat",
+"Slate Warrior",
+"Skull Mark Ladybug",
+"Des Kangaroo",
+"Des Koala",
+"Giant Rat",
 "Cybernetic Cyblopean",
 "Cyber Dragon",
-"Trap master",
-"Ancient gear beast",
-"Ancient gear soldier",
+"Trap Master",
+"Ancient Gear Beast",
+"Ancient Gear Soldier",
 "Blade Rabbit",
 "Bowganian",
 "Chaos Necomancer",
-"Magician of faith",
-"Swam of scarabs",
-"Man-eater bug",
-"Moai interceptor cannons",
-"Theban nightmare",
-"Yomi ship",
+"Magician of Faith",
+"Swam of Scarabs",
+"Man-eater Bug",
+"Moai Interceptor Cannons",
+"Theban Nightmare",
+"Yomi Ship",
 "Gear Golem the moving fortress",
 "Luster Dragon",
-"Arsenal bug",
+"Arsenal Bug",
 "Witch of the black forest",
-"The fiend megacyber",
-"Shadow ghoul",
+"The Fiend Megacyber",
+"Shadow Ghoul",
 "Shadowslayer",
 "Kiseitai",
 "Kuriboh",
-"gyaku-gire panda",
+"Gyaku-gire Panda",
 "Copycat",
-"Cobra jar",
-"Creeping doom",
-"Cure mermaid",
-"Jinzo #7"
+"Cobra Jar",
+"Creeping Doom",
+"Cure Mermaid",
+"Jinzo #7",
+"Nimble Momonga"
 ]
 
 Monster_copy = []
@@ -59,32 +62,27 @@ while len(Monster_copy) < len(Monster):
 
 Trap = [
 "D.D. Trap Hole",
-"Threatening roar",
-"Spirit barrier",
-"Draining shield",
-"Trap jammer",
-"Compulsory evacuation device",
-"Raigeki break",
-"Sakuretsu armor",
-"Bottomless trap hole",
-"Nightmare wheel",
-"Shadow spell",
-"Negate attack",
-"Trap hole",
-"Magic Jammer",
-"Secen tools of the bandit",
-"Robbin goblin",
-"mirrot force",
-"Numinous healer",
-"minor goblin official",
-"Time seal",
+"Threatening Roar",
+"Spirit Barrier",
+"Draining Shield",
+"Trap Jammer",
+"Compulsory Evacuation Device",
+"Raigeki Break",
+"Sakuretsu Armor",
+"Bottomless Trap Hole",
+"Nightmare Wheel",
+"Shadow Spell",
+"Trap Hole",
+"Secen Tools of The Bandit",
+"Robbin Goblin",
+"Numinous Healer",
+"Minor Goblin Official",
+"Time Seal",
 "Dust Tornado",
 "Enchanted Javelin",
-"Torrential tribute",
 "Enchanted Javelin",
-"Magic jammer",
+"Magic Jammer",
 "Negate attack",
-"Mirror Force",
 "Divine wrath",
 "Mask of restrict",
 "Waboku"
@@ -103,37 +101,35 @@ Spell = [
 "Axe of Despair",
 "Black Pendant",
 "Stop Defense",
-"Stop attack",
+"Block Attack",
 "Raigeki",
 "Ookazi",
 "Nightmare steelcage",
 "Megamorph",
 "Scape goat",
-"Monster Reborn",
 "Change of Heart",
 "Heavy Storm",
 "Mystical space typhoon",
 "Giant Turnade",
-"Fairy meteor crush",
-"Meteor of destruction",
+"Fairy Meteor Crush",
+"Meteor of Destruction",
 "Lightning Vortex",
 "Dark snake syndrome",
 "Monster reaincarnation",
-"Dark Hole",
 "Dian Keto the cure master",
-"De-spell",
-"United we stand",
-"Spring of rebirth",
-"Emergency provisions",
-"Autonomous action unit",
-"Smashing ground",
-"Mystic wok",
-"Goblin thief",
-"Soul reversal",
-"Hammer shot",
-"Wave motion cannon",
-"Block Attack"
-"Block Deffence"
+"De-Spell",
+"United We Stand",
+"Spring of Rebirth",
+"Emergency Provisions",
+"Autonomous Action Unit",
+"Smashing Ground",
+"Mystic Wok",
+"Goblin Thief",
+"Soul Reversal",
+"Hammer Shot",
+"Wave Motion Cannon",
+"Fissure",
+"Remove Trap"
 ]
 
 Spell_copy = []
@@ -152,6 +148,16 @@ print("Monster:"+str(Monster_count))
 print("Trap:"+str(Trap_count))
 print("Spell:"+str(Spell_count))
 
+while Monster_count>len(Monster):
+	print("Error: Insuficient Monster list!")
+	print("Ajusting monster amounth.")
+	Monster_count-=1
+	
+while Trap_count>len(Trap):
+	print("Error: Insuficient Monster list!")
+	print("Ajusting trap amounth.")
+	Trap_count-=1
+
 Deck_Monster=[]
 Deck_Trap=[]
 Deck_Spell=[]
@@ -159,7 +165,9 @@ Deck_Spell=[]
 #Monster
 for Card in range(Monster_count):
 	Card_id = random(len(Monster))
-
+	#Limit card copy
+	while Monster_copy[Card_id] >= 3:
+		Card_id = random(len(Monster))
 	if Monster_copy[Card_id] <= 3:
 		Deck_Monster.append(Monster[Card_id])
 		Monster_copy[Card_id]+=1
@@ -167,19 +175,26 @@ for Card in range(Monster_count):
 #Trap
 for Card in range(Trap_count):
 	Card_id = random(len(Trap))
-
+	#Limit card copy
+	while Trap_copy[Card_id] >= 3:
+		Card_id = random(len(Trap))
 	if Trap_copy[Card_id] <= 3:
 		Deck_Trap.append(Trap[Card_id])
 		Trap_copy[Card_id]+=1
 #Spell
 for Card in range(Spell_count):
 	Card_id = random(len(Spell))
-
+	#Limit card copy
 	if Spell_copy[Card_id] <= 3:
 		Deck_Spell.append(Spell[Card_id])
 		Spell_copy[Card_id]+=1
 
-
+# Include Limited Card
+if Limited == True:
+	for Card in range(len(Limited_spell)):
+		Deck_Spell[random(len(Deck_Spell))] = Limited_spell[Card]
+	for Card in range(len(Limited_trap)):
+		Deck_Trap[random(len(Deck_Trap))] = Limited_trap[Card]
 
 #Create Deck
 Deck_Monster.sort()
@@ -196,12 +211,11 @@ print(Deck)
 
 Deck_name = str(input("Deck name? :"))
 if Deck_name == "":
-    Deck_name = "Deck_"+str(random(9))+str(random(9))+str(random(9))+str(random(9))
+	Deck_name = "Deck_"+str(random(9))+str(random(9))+str(random(9))+str(random(9))
 
-with open("Deck"+Deck_name+".txt", "w") as file:
+with open("Deck/"+Deck_name+".txt", "w") as file:
 	# Writing data to a file
 	# file.write(str(Deck))
 	for Card in Deck:
 		file.writelines(Card+"\n\n")
-
 
